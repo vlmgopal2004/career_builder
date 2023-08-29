4c9ea8f10a4a356af4acfb110977ff9afede08be
vlmgopal2004 <vlmgopal2004@gmail.com>
2023-08-29 15:55:31 +0530

    new

--- app_server/controller/locations.js
+++ app_server/controller/locations.js
@@ -1,14 +1,14 @@
 /* GET 'home' page */
 const homelist = (req, res) => {
-    res.render('index', { title: 'Home' });
+    res.render('locations-list', { title: 'CareerBuilder' });
    };
    /* GET 'Location info' page */
    const locationInfo = (req, res) => {
-    res.render('index', { title: 'Location info' });
+    res.render('locations-info', { title: 'Location info' });
    };
    /* GET 'Add review' page */
    const addReview = (req, res) => {
-    res.render('index', { title: 'Add review' });
+    res.render('location-review-form', { title: 'Add review' });
    };
    module.exports = {
     homelist,
